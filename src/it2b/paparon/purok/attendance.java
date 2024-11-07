afb66fe48bea522141f3fa4d0d824edfb7fb923b
Dan <dandanhandsome1231@gmail.com>
2024-11-07 14:03:18 +0800

    aws

--- src/it2b/paparon/purok/attendance.java
+++ src/it2b/paparon/purok/attendance.java
@@ -19,14 +19,14 @@ public class attendance {
             System.out.println("5. Back to Main Menu");
             System.out.print("Enter choice: ");
             choice = sc.nextInt();
-            sc.nextLine();  
+            sc.nextLine();
 
             switch (choice) {
                 case 1:
                     recordAttendance(sc);
                     break;
                 case 2:
-                    viewAttendance(sc);
+                    viewAllAttendance(sc);
                     break;
                 case 3:
                     updateAttendance(sc);
@@ -43,65 +43,63 @@ public class attendance {
         } while (choice != 5);
     }
 
-    private static void recordAttendance(Scanner sc) {
+   private static void recordAttendance(Scanner sc) {
+    System.out.print("Enter the number of members to record attendance for: ");
+    int numberOfMembers = sc.nextInt();
+    sc.nextLine(); 
+
+    config conf = new config();
+
+    for (int i = 0; i < numberOfMembers; i++) {
+        System.out.println("Recording attendance for member " + (i + 1) + ":");
+        
         System.out.print("Enter Member ID: ");
         int memberId = sc.nextInt();
-        sc.nextLine();
+        sc.nextLine(); 
+        
         System.out.print("Enter Activity ID: ");
         int activityId = sc.nextInt();
-        sc.nextLine();
-        System.out.print("Enter Attendanc   e Status: ");
+        sc.nextLine(); 
+        
+        System.out.print("Enter Attendance Status: ");
         String status = sc.nextLine();
 
-        config conf = new config();
-        String sql = "INSERT INTO tbl_attendance (m_id, a_id,  s_status) VALUES (?, ?, ?)";
+        String sql = "INSERT INTO tbl_attendance (s_hid, s_id, s_status) VALUES (?, ?, ?)";
         conf.addRecord(sql, memberId, activityId, status);
-        
-        System.out.println("Attendance recorded successfully.");
+
+        System.out.println("Attendance recorded successfully for member " + (i + 1) + ".");
     }
+}
 
-    private static void viewAttendance(Scanner sc) {
 
-        
-            viewAllAttendance();
-         
-        }
-    
-
- private static void viewAllAttendance() {
+   private static void viewAllAttendance(Scanner sc) {
     String sqlQuery = "SELECT * FROM tbl_attendance "
-            + "JOIN tbl_members ON tbl_attendance.s_hid = tbl_members.s_hid "
-            + "JOIN tbl_activities ON tbl_attendance.s_id = tbl_activities.s_id";
-    
+                    + "JOIN tbl_members ON tbl_attendance.a_id = tbl_members.s_hid "
+                    + "JOIN tbl_activities ON tbl_attendance.a_id = tbl_activities.s_id";
+
     String[] attendanceHeaders = {"Attendance ID", "First Name", "Last Name", "Date", "Status"};
-    String[] attendanceColumns = {"a_id", "s_fname", "s_lastname", "s_date", "status"};
+    String[] attendanceColumns = {"a_id", "s_fname", "s_lastname", "s_date", "s_status"};
 
     config conf = new config();
     conf.viewRecords(sqlQuery, attendanceHeaders, attendanceColumns);
 }
-    
 
-   
 
     private static void updateAttendance(Scanner sc) {
         System.out.print("Enter the Attendance ID to Update: ");
         int id = sc.nextInt();
         sc.nextLine();
-        System.out.print("Enter new Member ID: ");
-        int memberId = sc.nextInt();
-        sc.nextLine();
-        System.out.print("Enter new Activity ID: ");
-        int activityId = sc.nextInt();
-        sc.nextLine();
-        System.out.print("Enter new Date: ");
-        String date = sc.nextLine();
-        System.out.print("Enter new Status: ");
-        String status = sc.nextLine();
+        System.out.print("Enter new Activity Name: ");
+        String activityName = sc.nextLine();
+        System.out.print("Enter new Location: ");
+        String location = sc.nextLine();
 
-        String qry = "UPDATE tbl_attendance SET m_id = ?, a_id = ?, s_date = ?, s_status = ? WHERE s_id = ?";
+        String qry = "UPDATE tbl_attendance "
+                   + "SET a_name = ?, location = ? "
+                   + "WHERE s_id = ?";
         config conf = new config();
-        conf.updateRecord(qry, memberId, activityId, date, status, id);
-        
+        conf.updateRecord(qry, activityName, location, id);
+
         System.out.println("Attendance updated successfully.");
     }
 
@@ -112,7 +110,7 @@ public class attendance {
         String qry = "DELETE FROM tbl_attendance WHERE s_id = ?";
         config conf = new config();
         conf.deleteRecord(qry, id);
-        
+
         System.out.println("Attendance record deleted successfully.");
     }
 }
